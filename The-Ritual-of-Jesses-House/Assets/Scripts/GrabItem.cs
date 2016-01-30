8ba77d9f8de8cfa4a95b81c7716d728fb7ffd626
Jesse Lathey <jalathey@fullsail.edu>
2016-01-30 16:27:44 -0500

    Added Storm Audio Controller

---
+++ The-Ritual-of-Jesses-House/Assets/Scripts/GrabItem.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GrabItem : MonoBehaviour
+{
+
+    Inventory inv;
+
+    void Awake()
+    {
+        inv = GetComponent<Inventory>();
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnCollisionStay(Collision _col)
+    {
+        if (_col.gameObject.tag == "Pickups" && Input.GetKeyDown(KeyCode.E))
+            inv.AddItem(_col.gameObject);
+    }
+}
