a4d4004917980d98b293da8a1ca401c57b429115
Richard <leaper96@gmail.com>
2016-01-30 01:31:32 -0500

    Rain and thunder audio, beginning of movement

---
+++ The-Ritual-of-Jesses-House/Assets/charaController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class charaController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetAxis ("Horizontal") != 0) {
+			GetComponent<Rigidbody> ().velocity = new Vector3 (Input.GetAxis ("Horizontal"), GetComponent<Rigidbody> ().velocity.y, GetComponent<Rigidbody> ().velocity.z);
+		}
+		if (Input.GetAxis ("Vertical") != 0) {
+			GetComponent<Rigidbody> ().velocity = new Vector3 (GetComponent<Rigidbody> ().velocity.x, GetComponent<Rigidbody> ().velocity.y, Input.GetAxis ("Vertical"));
+		}
+	}
+}
