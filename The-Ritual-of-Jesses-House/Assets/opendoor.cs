5e931891a111593663ace8cb38c78cd322112b58
Ryan Gilpin <ryan_gilpin@yahoo.com>
2016-01-30 20:43:44 -0500

    added opening doors as well as locking and unlocking with keys

---
+++ The-Ritual-of-Jesses-House/Assets/opendoor.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class opendoor : MonoBehaviour, IUsable {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void Use(GameObject Player){
+		gameObject.transform.Rotate(Vector3.right);
+	}
+}
