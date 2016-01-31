493a7656ce1d85758d6282595ffbf69f08f32c30
Brandon <bjsvochak@fullsail.edu>
2016-01-30 23:13:06 -0500

    Implemented Thieving Rats

---
+++ The-Ritual-of-Jesses-House/Assets/NymphController.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NymphController : MonoBehaviour 
+{
+    public NavMeshAgent navMeshAgent;
+    private Vector3 targetLocation;
+    private Quaternion targetRotation;
+    private GameObject carriedObject;
+    private Vector3 exitLocation;
+
+    void Awake()
+    {
+        exitLocation = transform.position;
+    }
+
+    void Update()
+    {
+        // massive nested statement to take care of nav mesh agent
+        if (!navMeshAgent.pathPending)
+        {
+            if (navMeshAgent.remainingDistance <= navMeshAgent.stoppingDistance)
+            {
+                if (!navMeshAgent.hasPath || navMeshAgent.velocity.sqrMagnitude == 0.0f)
+                {
+                    if(carriedObject != null)
+                    {
+                        // get rid of the object, no longer carrying it
+                        carriedObject.transform.SetParent(null);
+                        carriedObject.transform.position = targetLocation;
+                        carriedObject.transform.rotation = targetRotation;
+                        carriedObject = null;
+
+                        navMeshAgent.gameObject.tag = "Untagged";
+                        navMeshAgent.SetDestination(exitLocation);
+                    }
+                    else
+                    {
+                        // has reached destination for exit
+                        navMeshAgent.gameObject.SetActive(false);
+                    }
+                }
+            }
+        }
+    }
+    void OnTriggerEnter(Collider coll)
+    {
+        Pickupable pickup = coll.gameObject.GetComponent<Pickupable>();
+
+        if (pickup == null)
+            return;
+
+        if(pickup.NymphPickup(gameObject))
+        {
+            navMeshAgent.Stop();
+            navMeshAgent.ResetPath();
+
+            targetLocation = pickup.GetNymphDestination();
+            targetRotation = pickup.GetOriginalRotation();
+            carriedObject = pickup.gameObject;
+
+            navMeshAgent.SetDestination(pickup.GetNymphDestination());
+
+            navMeshAgent.gameObject.tag = "Carrying";
+        }
+    }
+}
---
+++ The-Ritual-of-Jesses-House/Assets/NymphSpawning.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NymphSpawning : MonoBehaviour 
+{
+    public GameObject[] nymphs;
+    public GameObject[] waypoints;
+    public void Spawn(Vector3 _pickupLoc, Vector3 _returnLoc)
+    {
+        foreach(GameObject nymph in nymphs)
+        {
+            if (nymph.tag == "Carrying")
+                continue;
+        
+            nymph.SetActive(true);
+            NavMeshAgent agent = nymph.GetComponent<NavMeshAgent>();
+            agent.Stop();
+            agent.ResetPath();
+        
+            Vector3 farthestPoint = _pickupLoc;
+            foreach(GameObject waypoint in waypoints)
+            {
+                if(Vector3.Distance(_pickupLoc, farthestPoint) < Vector3.Distance(_pickupLoc, waypoint.transform.position))
+                    farthestPoint = waypoint.transform.position;
+            }
+        
+            nymph.transform.position = farthestPoint;
+            agent.SetDestination(_pickupLoc);
+        
+            break;
+        }
+        
+    }
+}
