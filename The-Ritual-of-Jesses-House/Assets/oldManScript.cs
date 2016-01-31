bdc8a9e5cc321060b5e84ce137d088dcbc25af25
Richard <leaper96@gmail.com>
2016-01-31 12:31:54 -0500

    Fixed prefabs

---
+++ The-Ritual-of-Jesses-House/Assets/oldManScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class oldManScript : MonoBehaviour {
+
+	TextBubble bub;
+
+	// Use this for initialization
+	void Start () {
+		bub = GetComponent<TextBubble> ();
+		bub.SetString ("Find the thing!");
+		Invoke ("FlyAway", 5f);
+	}
+
+	void FlyAway () {
+		GetComponent<Rigidbody> ().velocity = new Vector3 (0, 5, 0);
+		Invoke ("Despawn", 3f);
+	}
+
+	void Despawn()
+	{
+		Destroy (gameObject);
+	}
+}
