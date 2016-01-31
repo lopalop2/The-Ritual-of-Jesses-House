c77fbb53157a7a7fa03c728cd4b9bdedd10cd1f8
David <dwilcox@fullsail.edu>
2016-01-31 15:13:37 -0500

    Ritual Pickupable and Win Condition scripts created

---
+++ The-Ritual-of-Jesses-House/Assets/Scripts/RitualController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RitualController : MonoBehaviour
+{
+    public int numRitualItems;
+    private int itemsPlaced = 0;
+
+    public GameObject WinScreen;
+
+    public void ItemComplete()
+    {
+        itemsPlaced++;
+
+        if(itemsPlaced >= numRitualItems)
+        {
+            EndGame();
+        }
+    }
+
+    public void ItemRemoved()
+    {
+        itemsPlaced--;
+    }
+
+    void EndGame()
+    {
+        WinScreen.SetActive(true);
+        Time.timeScale = 0.0f;
+    }
+
+}
