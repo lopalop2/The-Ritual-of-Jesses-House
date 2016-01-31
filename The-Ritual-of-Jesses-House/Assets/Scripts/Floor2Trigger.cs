d6a27f95dd842e42aee3db059c58cf62687287f1
Jesse Lathey <jalathey@fullsail.edu>
2016-01-31 13:16:09 -0500

    FixedStairs

--- The-Ritual-of-Jesses-House/Assets/Scripts/Floor2Trigger.cs
+++ The-Ritual-of-Jesses-House/Assets/Scripts/Floor2Trigger.cs
@@ -8,20 +8,12 @@ public class Floor2Trigger : MonoBehaviour
     [SerializeField]
     GameObject Floor2 = null;
 
-    bool isFloor2 = false;
-
     //Make sure player is tagged as such!
     void OnTriggerEnter(Collider _col)
     {
-        if (_col.gameObject.tag == "Player" && !isFloor2)
+        if (_col.gameObject.tag == "Player")
         {
             Floor2.SetActive(true);
-            isFloor2 = true;
-        }
-        else if(_col.gameObject.tag == "Player" && isFloor2)
-        {
-            Floor2.SetActive(false);
-            isFloor2 = false;
         }
     }
 }
